4cf2eb1ff7fb05de827be9de86a72388045c136b
reubenrandell <reubenr2121@gmail.com>
2021-10-13 19:24:11 -0500

    initial commit

---
+++ android/app/src/main/kotlin/edu/uco/rrandell/mobileapps/lesson3/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.rrandell.mobileapps.lesson3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:lesson3/model/constant.dart';
+import 'package:lesson3/viewscreen/signin_screen.dart';
+
+void main() {
+  runApp(Lesson3App());
+}
+
+class Lesson3App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: Constant.DEV,
+      initialRoute: SignInScreen.routeName,
+      routes: {
+        SignInScreen.routeName: (context) => SignInScreen(),
+      }
+    );
+  }
+  
+}
